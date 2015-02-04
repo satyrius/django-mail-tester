2223ecf9608ee5a749042e89795b6b0e5236877a
Anton Egorov <anton.egoroff@gmail.com>
2015-02-04 20:43:18 +0300

    sendmail command

--- django_mail_tester/management/commands/sendmail.py
+++ django_mail_tester/management/commands/sendmail.py
@@ -1,5 +1,31 @@
+import datetime
+from optparse import make_option
+
+from django.conf import settings
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    pass
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '-e', '--email', '--email-to', action='store', dest='email',
+            help='Email to send to'),
+        make_option(
+            '-f', '--from', '--email-from', action='store', dest='from',
+            help='Email to send from. It is SERVER_EMAIL by default'),
+        make_option(
+            '-s', '--subject', action='store', dest='subject',
+            default='Test email by sendmail command', help='Email subject'),
+    )
+
+    def handle(self, *args, **options):
+        message = u' '.join(args)
+        if not message:
+            message = 'Test email sending'
+        message += u'\nGenerated at {now}'.format(now=datetime.datetime.now())
+
+        email_to = options['email']
+        assert email_to
+        email_from = options['from'] or settings.SERVER_EMAIL
+        send_mail(options['subject'], message, email_from, [email_to])
